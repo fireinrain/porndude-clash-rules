ddf28b70365cb90a28a6820f0d990cf71f1fe149
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-04-04 00:30:53 +0800

    update

--- main.py
+++ main.py
@@ -1,82 +1,125 @@
 import asyncio
+from urllib.parse import urlparse
 
 import aiohttp
+from bs4 import BeautifulSoup
 
-porndude_sites = ['https://theporndude.vip/',
-                  'https://pornsites.com/', 'https://porngeek.com/']
+
+import asyncio
+from urllib.parse import urlparse
+import aiohttp
+from bs4 import BeautifulSoup
 
 
 async def fetch_index_page(url: str) -> {}:
     headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-        'Referer': 'https://www.google.com/'
     }
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
             if response.status == 200:
                 resp = await response.text()
-                print(resp)
+                # print(resp)
                 return {'url': url, 'data': resp}
             else:
                 error = ValueError(f"Failed to fetch data from {url}, status code: {response.status}")
                 return {'url': url, 'data': error}
 
 
-async def fetch_multiple_urls(urls):
-    tasks = [fetch_index_page(url) for url in urls]
-    results = await asyncio.gather(*tasks)
-    return results
-
-
-def extract_url_from_pornsites(page_str: str) -> []:
-    pass
-
-def extract_url_from_theporndude(page_str: str) -> []:
-    pass
-
-def extract_url_from_porngeek(page_str: str) -> []:
-    pass
-
-
-def extract_dynamic_resource(page_str: str) -> []:
-    # the porndude 默认只有一部分是静态页面展示网址
-    # 还有一部分是靠js加在出来
-    # <script type="text/javascript" src="https://theporndude.com/includes/pi/en.1712086370.passive.info.js"></script>
-    # var passive_skip_data = [];
-    # var passive_index_file = 'en.1712086370.passive.idx.json';
-    # var passive_index_cat_file = 'en.1712086370.passive.mobi.cat.json';
-    # var passive_index_mobi_files = ['en.1712086370.passive.mobi.0.json','en.1712086370.passive.mobi.1.json','en.1712086370.passive.mobi.2.json','en.1712086370.passive.mobi.3.json','en.1712086370.passive.mobi.4.json','en.1712086370.passive.mobi.5.json','en.1712086370.passive.mobi.6.json','en.1712086370.passive.mobi.7.json','en.1712086370.passive.mobi.8.json','en.1712086370.passive.mobi.9.json','en.1712086370.passive.mobi.10.json'];
-    # 但是目前通过header + cookie的方式无法访问到默认的页面
-    # 默认页面上了cloudflare cdn 无法直接请求
-    # 这部分暂不实现
-    pass
-
-
-def generate_clash_rules(sites: []):
-    # 默认使用DOMAIN-SUFFIX 来匹配域名
-    pass
+async def fetch_redirected_url(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, allow_redirects=True, headers=headers) as response:
+            return response.url
 
 
 async def main():
-    pornsites_resp = await fetch_multiple_urls(porndude_sites)
-    all_site = []
-    for site in pornsites_resp:
-        if 'theporndude' in site['url']:
-            urls = extract_url_from_theporndude(site['data'])
-            all_site.append(urls)
-            continue
-        if 'porngeek' in site['url']:
-            urls = extract_url_from_porngeek(site['data'])
-            all_site.append(urls)
-            continue
-        if '' in site['url']:
-            urls = extract_url_from_pornsites(site['data'])
-            all_site.append(urls)
+    sites_ = await fetch_index_page("https://theporndude.vip")
+    # print(sites_)
+    soup = BeautifulSoup(sites_['data'], 'html.parser')
 
-    generate_clash_rules(all_site)
+    a_tags = soup.find_all('a')
+    target_links = []
+    for a in a_tags:
+        href = a.get('href')
+        # make http to https
+        if 'https://' in href:
+            target_links.append(href)
+            continue
+        if 'http://' in href:
+            new_href = href.replace('http://', 'https://')
+            target_links.append(new_href)
+            continue
+        # print(href)
+    print(f"源链接数量: {len(a_tags)}")
+    print(f"处理后的连接数量: {len(target_links)}")
+    # print(all_site_div)
 
+    # 先进行去重处理
+    s = set(target_links)
+    all_links = list(s)
+    print(f"去重后的连接数量: {len(s)}")
+
+    domain_sets = set()
+    url_size = len(all_links)
+    for index, link in enumerate(all_links):
+        print(f"正在处理第{index + 1} 个链接, 还有: {url_size - index - 1},共有: {url_size}个链接...")
+        parsed_url = urlparse(link)
+        domain = parsed_url.netloc
+        if domain == 'theporndude.vip':
+            # 需要二次请求获取302跳转的地址
+            real_link = await fetch_redirected_url(link)
+            await asyncio.sleep(0.2)
+            resp_data = await fetch_index_page(real_link)
+            soup = BeautifulSoup(resp_data['data'], 'html.parser')
+            aa_tags = soup.find_all('a')
+            for aa in aa_tags:
+                href = aa.get('href')
+                # make http to https
+                if href is not None and 'https://' in href:
+                    parsed_url = urlparse(href)
+                    domain = parsed_url.netloc
+                    domain_sets.add(domain)
+                    continue
+                if href is not None and 'http://' in href:
+                    new_href = href.replace('http://', 'https://')
+                    parsed_url = urlparse(new_href)
+                    domain = parsed_url.netloc
+                    domain_sets.add(domain)
+                    continue
+        else:
+            # 直接加入domain 列表
+            domain_sets.add(domain)
+        print(f'当前已获得域名数量: {len(domain_sets)}')
+    # write domain set to a text file
+    with open('porndude.txt', 'w+') as f:
+        sites_str = '\n'.join(list(domain_sets))
+        f.write(sites_str)
+        f.flush()
+    print(f"写入完成")
 
 
 if __name__ == '__main__':
     asyncio.run(main())
+    # from urllib.parse import urlparse
+    #
+    #
+    # def get_domain_from_url(url):
+    #     parsed_url = urlparse(url)
+    #     domain = parsed_url.netloc
+    #     return domain
+    #
+    #
+    # # Example usage:
+    # url = 'https://www.example.com/path/to/page'
+    # domain = get_domain_from_url(url)
+    # print("Domain:", domain)
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,155 @@
+import asyncio
+from urllib.parse import urlparse
+
+import aiohttp
+from bs4 import BeautifulSoup
+porndude_sites = ['https://theporndude.vip',
+                  'https://pornsites.com', 'https://porngeek.com']
+
+
+async def fetch_index_page(url: str) -> {}:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                resp = await response.text()
+                # print(resp)
+                return {'url': url, 'data': resp}
+            else:
+                error = ValueError(f"Failed to fetch data from {url}, status code: {response.status}")
+                return {'url': url, 'data': error}
+
+
+async def fetch_multiple_urls(urls):
+    tasks = [fetch_index_page(url) for url in urls]
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+async def extract_url_from_pornsites(link: str, page_str: str) -> []:
+    soup = BeautifulSoup(page_str, 'html.parser')
+    find_all = soup.find_all('a')
+    site_urls = []
+    for i in find_all:
+        href = i.get('href')
+        if '/review/' in href:
+            continue
+        if 'https://' in href:
+            continue
+        if '/' == href:
+            continue
+        if '/blog' == href:
+            continue
+        if '/' == href:
+            continue
+        if '/about' == href:
+            continue
+        if '/terms' == href:
+            continue
+        if '/privacy' == href:
+            continue
+        if '/disclaimer' == href:
+            continue
+        print(href)
+        site_urls.append(href)
+    results = []
+    for url in site_urls:
+        part_sites = f'{link}{url}'
+        collect_page = await fetch_index_page(part_sites)
+        beautiful_soup = BeautifulSoup(collect_page['data'], 'html.parser')
+        all_site_div = beautiful_soup.find_all('a')
+        await asyncio.sleep(3)
+        print(all_site_div)
+    await asyncio.sleep(0.01)
+    return results
+
+
+async def extract_url_from_theporndude(link: str, page_str: str) -> []:
+    results = []
+
+    soup = BeautifulSoup(page_str, 'html.parser')
+    find_all = soup.find_all('a')
+    site_urls = []
+    for i in find_all:
+        href = i.get('href')
+        if '/review/' in href:
+            continue
+        if 'https://' or 'http' in href:
+            parsed_url = urlparse(href)
+            domain = parsed_url.netloc
+            results.append(domain)
+            continue
+        if '/' == href:
+            continue
+        if '/blog' == href:
+            continue
+        if '/' == href:
+            continue
+        if '/about' == href:
+            continue
+        if '/terms' == href:
+            continue
+        if '/privacy' == href:
+            continue
+        if '/disclaimer' == href:
+            continue
+        print(href)
+        site_urls.append(href)
+    # for url in site_urls:
+    #     part_sites = f'{link}{url}'
+    #     collect_page = await fetch_index_page(part_sites)
+    #     beautiful_soup = BeautifulSoup(collect_page['data'], 'html.parser')
+    #     all_site_div = beautiful_soup.find_all('a')
+    #     await asyncio.sleep(3)
+    #     print(all_site_div)
+    # await asyncio.sleep(0.01)
+    return results
+
+
+def extract_url_from_porngeek(link: str, page_str: str) -> []:
+    pass
+
+
+def extract_dynamic_resource(page_str: str) -> []:
+    # the porndude 默认只有一部分是静态页面展示网址
+    # 还有一部分是靠js加在出来
+    # <script type="text/javascript" src="https://theporndude.com/includes/pi/en.1712086370.passive.info.js"></script>
+    # var passive_skip_data = [];
+    # var passive_index_file = 'en.1712086370.passive.idx.json';
+    # var passive_index_cat_file = 'en.1712086370.passive.mobi.cat.json';
+    # var passive_index_mobi_files = ['en.1712086370.passive.mobi.0.json','en.1712086370.passive.mobi.1.json','en.1712086370.passive.mobi.2.json','en.1712086370.passive.mobi.3.json','en.1712086370.passive.mobi.4.json','en.1712086370.passive.mobi.5.json','en.1712086370.passive.mobi.6.json','en.1712086370.passive.mobi.7.json','en.1712086370.passive.mobi.8.json','en.1712086370.passive.mobi.9.json','en.1712086370.passive.mobi.10.json'];
+    # 但是目前通过header + cookie的方式无法访问到默认的页面
+    # 默认页面上了cloudflare cdn 无法直接请求
+    # 这部分暂不实现
+    pass
+
+
+def generate_clash_rules(sites: []):
+    # 默认使用DOMAIN-SUFFIX 来匹配域名
+    pass
+
+
+async def main():
+    pornsites_resp = await fetch_multiple_urls(porndude_sites)
+    all_site = []
+    for site in pornsites_resp:
+        if 'theporndude' in site['url']:
+            urls = await extract_url_from_theporndude(site['url'], site['data'])
+            all_site.append(urls)
+            continue
+        if 'porngeek' in site['url']:
+            urls = extract_url_from_porngeek(site['url'], site['data'])
+            all_site.append(urls)
+            continue
+        if 'pornsites' in site['url']:
+            pass
+            # urls = await extract_url_from_pornsites(site['url'], site['data'])
+            # all_site.append(urls)
+
+    generate_clash_rules(all_site)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
