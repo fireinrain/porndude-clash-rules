d6a09d15690a438eeab55e7b0cd35f7346cbeeed
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-04-04 12:23:20 +0800

    update

--- main.py
+++ main.py
@@ -198,5 +198,5 @@ def generate_clash_rule(file_name: str, domains: []):
 
 
 if __name__ == '__main__':
-    # asyncio.run(main())
-    asyncio.run(add_porndude_static_source('PornDude-zh.html'))
+    asyncio.run(main())
+    # asyncio.run(add_porndude_static_source('PornDude-zh.html'))
